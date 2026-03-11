eb6c571c1538dc3a5211c99955a653026cef372a
bullas <bullas.atk@gmail.com>
2026-03-11 16:26:27 -0300

    update title

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,7 +15,7 @@ export function App() {
             className={`sidebar__link ${activePage === "tasks" ? "is-active" : ""}`}
             onClick={() => setActivePage("tasks")}
           >
-            Minhas Tarefas 3
+            Minhas Tarefas 4
           </button>
           <button
             type="button"
